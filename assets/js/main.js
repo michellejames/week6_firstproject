af41a89d56d8977617742e08bd9c854013dc20bf
michellejames <shellyjames90@yahoo.com>
2017-05-11 18:09:23 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+console.log("Hello World from main.js!");
+
+TweenMax.from(".headline", 1.5,{top:"-100px", opacity:"0", overwrite:"none"});
+
+TweenMax.from(".header", 1.5,{top:"-100px", opacity:"0", overwrite:"none"});
